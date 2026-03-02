701141db2ed5ec0ed8259ffa24d45aeb9ecca509
APOLLON\antho <anthony.sapet@outlook.com>
2026-03-02 10:52:16 +0100

    EntityFramework With sqlite

---
+++ WebApplication1/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    
+    public  DbSet<User> Users { get; set; }
+}
